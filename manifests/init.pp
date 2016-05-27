9c841c96ff8dc6aeb9dcfdd427683a0978836e42
Simon Peeters <simon@inuits.eu>
2016-05-27 09:55:51 +0200

    add defaults

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,7 +13,37 @@ class nsswitch {
     ;
     "nsswitch.conf_footer":
       target  => '/etc/nsswitch.conf',
-      content => "\n\n",
+      content => "\n\n# End of /etc/nsswitch.conf\n",
       order   => "9_footer",
+  }
+
+  #these should be the glibc defaults
+  $defaults = {
+    passwd    => 'files',
+    group     => 'files',
+    shadow    => 'files',
+    networks  => 'files',
+    protocols => 'files',
+    services  => 'files',
+    ethers    => 'files',
+    rpc       => 'files',
+    netgroup  => 'files',
+  }
+  $defaults.each |$db, $value| {
+    nsswitch::entry {"default for ${db}":
+      database => $db,
+      value    => $value,
+    }
+  }
+  nsswitch::entry {
+    "default for hosts: files":
+      database => 'hosts',
+      value    => 'files',
+      order    => '40',
+    ;
+    "default for hosts: dns":
+      database => 'hosts',
+      value    => 'dns',
+      order    => '50',
   }
 }
